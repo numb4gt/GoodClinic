04d64641a9f1edb8500182dcbb70e39f3a036ce1
numb4gt <psharilov@mail.ru>
2022-10-26 16:38:46 +0300

    first commit

---
+++ src/main/java/by/belstu/bless/GoodClinicApplication.java
@@ -0,0 +1,22 @@
+package by.belstu.bless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class GoodClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodClinicApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate RestTemplate(){
+		return new RestTemplateBuilder().build();
+	}
+
+}
---
+++ src/test/java/by/belstu/bless/GoodClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package by.belstu.bless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
